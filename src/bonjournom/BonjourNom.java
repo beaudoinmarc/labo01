92f301c3824e2acb2692aff8f469b1f841276f30
Marc Beaudoin <michelmarcbeaudoin@gmail.com>
2021-05-20 19:36:00 -0400

    J'ai ajouté quelque chose pour faire un test.

---
+++ src/bonjournom/BonjourNom.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonjournom;
+import aleatoire.*;
+/**
+ *
+ * @author Marc Beaudoin
+ */
+public class BonjourNom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Person john = new Person(args[0], args[1], 46);
+        
+        System.out.println("Prenom :" + john.getFirstName());
+        System.out.println("Nom : " + john.getLastName());
+        System.out.println("Âge : " + john.getAge());
+        
+        
+    }
+    
+}
